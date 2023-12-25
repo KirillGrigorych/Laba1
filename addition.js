0a96e94a1994dd7bee40e25082b71cd0474db0bc
KirillGrigorych <135447172+KirillGrigorych@users.noreply.github.com>
2023-12-25 09:55:50 +0700

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,8 @@
+alert("Введите два значения, чтобы получить их сумму");
+let firstValue = prompt("Первое число:");
+let secondValue = prompt("Второе число:");
+firstValue = Number(firstValue);
+secondValue = Number(secondValue);
+let result = firstValue + secondValue;
+alert("Ответ: " + firstValue + "+" + secondValue + "=" + result);
+
---
+++ division.js
@@ -0,0 +1,7 @@
+alert("Введите два значения, чтобы получить их частное");
+let firstValue = prompt("Первое число:");
+let secondValue = prompt("Второе число:");
+firstValue = Number(firstValue);
+secondValue = Number(secondValue);
+let result = firstValue / secondValue;
+alert("Ответ: " + firstValue + ":" + secondValue + "=" + result); 
